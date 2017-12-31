d1c258565332dcbb80d899f9cfb7905a7a203310
thanhbao1996 <34533056+thanhbao1996@users.noreply.github.com>
2018-01-01 01:40:48 +0700

    Add files via upload

--- TrangChu.php
+++ TrangChu.php
@@ -13,27 +13,22 @@ hello my name í Bao
 	style="margin-top: 0px; background: #000000 url(images/bg.jpg); center top no-repeat fixed; background-size: 100%;">
 
 
+		<td width="650" valign="top"><!--load sách đồng thời xử lý phân trang-->
+		<?php
+		include 'content_page_home.php';
+		?></td>
+
+		<!-- Menu bên phải -->
+		<td width="300" valign="top"><?php 
+		include 'right_column.php';
+		?></td>
+	</tr>
+	
+
 <!-- thực hiện các kết nối đến cơ sở dữ liệu để thao tác -->
 <?php
 include 'config.php';
 ?>
-
-
-<table border="0" cellpadding="2" cellspacing="2"
-	style="border-collapse: collapse; margin-top: 0px;" width="1200px"
-	align="center">
-
-	<tr>
-		<td colspan="3" align="center" bgcolor="white"
-			style="background: #000000 url(images/banner.png); width: 1200px; height: 330px;"></td>
-	</tr>
-
-	<tr>
-		<td colspan="3" align="left" bgcolor="white"><!--thanh navbar--> <?php 
-		include 'navbar.php';
-		?></td>
-	</tr>
-
 	<tr style="background: #000000 url(images/bg_td.jpg)">
 
 
