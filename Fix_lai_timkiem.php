c27b764710fbf5b2c82b845a4c19d905390ee59f
Duong Van Hien <dvhien1997@gmail.com>
2018-01-01 19:42:30 +0700

    WEB-29 Fix lai tim kiem

---
+++ Fix_lai_timkiem.php
@@ -0,0 +1,13 @@
+
+<tr
+	style='background: url(images/bg_tr.jpg); background-size: 100% 100%;'>
+	<td align="left" style='padding: 5; width: 100%; height: 80'
+		bgcolor="white"><input type="text" name="noidung_timkiem"
+		style='background: url(images/bg_search.png); background-size: 100% 100%; padding: 15 15 15 15; width: 100%; height: 100%; font-size: 20; font-weight: bold; color: yellow;'
+		value="Tìm sách..."
+		onfocus="if (this.value == 'Tìm sách...') this.value=''" ... /></td>
+	<td bgcolor="white"><input type="submit"
+		style="width: 80px; height: 80px; background: url(images/search_button.png); background-size: 100% 100%;"
+		value=""></td>
+</tr>
+
