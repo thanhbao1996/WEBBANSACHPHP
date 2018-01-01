71e1b3fdeef9614952b5e9dcb9636c66ac1efada
bacoca <35005556+bacoca@users.noreply.github.com>
2018-01-02 00:01:50 +0700

    Quản lý sách

--- edit_sach.php
+++ edit_sach.php
@@ -352,9 +352,9 @@ echo "</table>";
 
 
 <tr>
-<td colspan="3" align="center" style="background-image:url('images/bg_footer.png'); background-size: 100% 150px;" height="150px"><b><font color="white" size="6pt" face="verdana,arial,sans-serif">Design & Develop By Group 5</font></b>
+<td colspan="3" align="center" style="background-image:url('images/bg_footer.png'); background-size: 100% 150px;" height="150px"><b><font color="white" size="6pt" face="verdana,arial,sans-serif">Design & Develop By Group 9</font></b>
 
-<p><b><font color="white" size="4pt" face="verdana,arial,sans-serif">Nha Trang University &reg; <?php echo date("Y");?></font></b></p>
+<p><b><font color="white" size="4pt" face="verdana,arial,sans-serif">HUTECH University &reg; <?php echo date("Y");?></font></b></p>
 
 </td>
 </tr>
