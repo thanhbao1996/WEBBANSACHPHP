986fe86c5487bcab0067133c7dfeffd5de8a5379
Van Loc <vanloc.thc@gmail.com>
2018-01-01 18:14:19 +0700

    chinh sua trang chu

---
+++ trangchu_fix.php
@@ -0,0 +1,64 @@
+﻿<?php
+session_start();
+?>
+<html>
+<head>
+<meta charset="utf-8">
+hello my name í Bao
+<title>Cua hang sach </title>
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+</head>
+
+<body
+	style="margin-top: 0px; background: #000000 url(images/bg.jpg); center top no-repeat fixed; background-size: 100%;">
+
+
+		<td width="650" valign="top"><!--load sách đồng thời xử lý phân trang-->
+		<?php
+		include 'content_page_home.php';
+		?></td>
+
+		<!-- Menu bên phải -->
+		<td width="300" valign="top"><?php 
+		include 'right_column.php';
+		?></td>
+	</tr>
+	
+
+<!-- thực hiện các kết nối đến cơ sở dữ liệu để thao tác -->
+<?php
+include 'config.php';
+?>
+	<tr style="background: #000000 url(images/bg_td.jpg)">
+
+
+		<!-- menu trái -->
+		<td width="250" valign="top" background="#FFFFFF"><?php 
+		include 'left_column.php';
+		?></td>
+
+
+		<td width="650" valign="top"><!--load sách đồng thời xử lý phân trang-->
+		<?php
+		include 'content_page_home.php';
+		?></td>
+
+		<!-- Menu bên phải -->
+		<td width="300" valign="top"><?php 
+		include 'right_column.php';
+		?></td>
+	</tr>
+
+
+
+	<?php
+	include 'footer.php';
+	?>
+
+</table>
+
+
+
+</body>
+
+</html>
